315940248e2b80bb99ac071c99054d9e27af9e20
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2020-10-10 16:49:37 +0300

    Update RunVisuals.py

--- Frontend/RunVisuals.py
+++ Frontend/RunVisuals.py
@@ -1,19 +1,23 @@
 import pygame
 from Dijkstra.Frontend.Block import *
 
+# mouse clicks
 LEFT_MOUSE_CLICK = 0
 MID_MOUSE_CLICK = 1
 RIGHT_MOUSE_CLICK = 2
+
+# screen resolution
 WIDTH = HEIGHT = 800
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Dijkstra")
+
+# colors (R,B,G)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 
 class RunVisuals:
-    def __init__(self, window):
-        self.win = window
+    def __init__(self, screen_width=WIDTH,screen_height=HEIGHT):
+        self.win = pygame.display.set_mode((screen_width, screen_height))
+        pygame.display.set_caption("Dijkstra")
         self.win.fill(WHITE)
         self.blocks = []
 
@@ -34,10 +38,10 @@ class RunVisuals:
                     run = False
             self.draw_all()
             if pygame.mouse.get_pressed()[LEFT_MOUSE_CLICK]:
-                x,y = pygame.mouse.get_pos()
-                self.blocks.append(Block(x,y))
+                x, y = pygame.mouse.get_pos()
+                self.blocks.append(Block(x, y))
 
 
 if __name__ == "__main__":
-    rv = RunVisuals(WIN)
+    rv = RunVisuals()
     rv.main()
